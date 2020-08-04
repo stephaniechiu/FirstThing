cb443295a8a713031294b4265b1f5dbe4494d0c6
Stephanie Chiu <stephaniechiu@outlook.com>
2020-08-04 13:48:17 -0700

    Initial Commit

---
+++ FirstThing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstThing
+//
+//  Created by Stephanie on 8/4/20.
+//  Copyright © 2020 Stephanie Chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
